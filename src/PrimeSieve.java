33ae6c6fc10720aeb12dbd015dae7a4412c4b25c
fawcej <fawcej@USCSFAWCEJL2C.corp.emc.com>
2017-09-12 17:01:37 -0600

    Incorrect Syntax in Java Application

--- src/PrimeSieve.java
+++ src/PrimeSieve.java
@@ -38,7 +38,7 @@ public class PrimeSieve {
     	System.out.println("Please enter a number:");
     	Scanner input = new Scanner(System.in);
     	//number = input.nextInt();
-    	number = 200;
+    	number = 200
     	
     	//Use supplied number as argument for prime method.
     	checkPrimeNumbers(number);
